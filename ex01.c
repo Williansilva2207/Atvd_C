6e88ef5c4add0d3e4e20fd79b54f9e02bd15b7ed
Willian Rodrigues <149281430+Williansilva2207@users.noreply.github.com>
2024-03-04 12:05:28 +0000

    Att

--- ex01.c
+++ ex01.c
@@ -3,31 +3,45 @@
 
 
 int main(){
-    int a,i,e;
+    int a,i,e, min, max;
     double b = 0;
     int c = 0;
     int ar[100];
     double media, desvio;
+    int posicao_vetorM,posicao_vetormen ;
     printf("Quantos números você quer somar?:\n");
     scanf("%d", &a);
-    for(i=1;i <=a; i++){
+    for(i=0;i <a; ++i){
         printf("Coloque %d número:\n", i);
         scanf("%d", &ar[i-1]);
         c += ar[i-1];
         
     }
     printf("Somatorio %d\n", c);
-    media = c/a;
+    media = (double)c/(double)a;
     printf("Media %.2lf\n", media);
-    for(e = 0; e< a; e++){
-        b += pow((ar[e]-media),2); 
+    for(i = 0; i< a; ++i){
+        b += pow((ar[i]-media),2); 
           
     }
     
     desvio = sqrt(b/a);
     printf("Desvio %lf", desvio);
     
-    
-
+    min = 0;
+    max = 0;
+    for (int i = 0; i < a; ++i) {
+        if (ar[i] < ar[min]) {
+            min = i;
+        } else if (ar[i] > ar[max]) {
+            max = i;
+        }
+    }
+            
+    printf("O menor número no vetor é %d\n", ar[min]);
+    printf("O maior número no vetor é %d\n", ar[max]);
+    printf("A posição do menor número é %d\n", min);
+    printf("A posição do maior número é %d\n", max);
+            
     return 0;
 }
